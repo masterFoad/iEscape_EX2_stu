4c1a10513bf64c1e9d617e4866a3fa2ddefae96e
SamerShahin <samer.shahin@galilsoftware.com>
2017-10-20 00:39:16 +0300

    LOGGED_RECEPTIONIST

--- src/gui/utils/TypeConstants.java
+++ src/gui/utils/TypeConstants.java
@@ -11,5 +11,7 @@ public class TypeConstants {
     public static final String INSTRUCTOR="instructor";
 
     public final static String ADMIN="Admin";
+    public final static String LOGGED_RECEPTIONIST="loggedReceptionist";
+
 
 }
