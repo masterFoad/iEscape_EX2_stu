03fb7cda0d3b2f7a03ec989e27da66aefc382917
Unknown <foad.ad5491@gmail.com>
2017-10-20 01:28:15 +0300

    commons

---
+++ src/gui/utils/Commons.java
@@ -0,0 +1,40 @@
+package gui.utils;
+
+import controller.SysData;
+
+public class Commons {
+
+    private Commons(){}
+
+
+        public static  boolean IsLoggedReceptionist(){
+
+            if(SysData.getInstance().getParameter(TypeConstants.LOGGED_RECEPTIONIST)!=null)
+            {
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+        public static boolean IsLoggedCustomer(){
+            if(SysData.getInstance().getParameter(TypeConstants.LOGGED_CUSTOMER)!=null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        public static boolean IsLoggedAdmin(){
+            if(SysData.getInstance().getParameter(TypeConstants.ADMIN)!=null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+    }
+
+
+}
